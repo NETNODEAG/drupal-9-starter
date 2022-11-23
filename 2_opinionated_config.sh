fe57e30613ffbfd5b66d5426a1db6b629b926d9b
lukas.fischer <lukas.fischer@netnode.ch>
2022-11-23 14:36:01 +0100

    optiniated gin

--- 2_opinionated_config.sh
+++ 2_opinionated_config.sh
@@ -1,4 +1,5 @@
 vendor/drush/drush/drush theme:enable gin
 vendor/drush/drush/drush config-set system.theme default gin -y
 vendor/drush/drush/drush config-set system.theme admin gin -y
+vendor/drush/drush/drush config-set gin.settings classic_toolbar horizontal -y
 vendor/drush/drush/drush config-set system.site page.front "/admin/content" -y

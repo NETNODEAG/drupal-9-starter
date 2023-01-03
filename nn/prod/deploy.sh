6816fe0cb5c4fb2f42bc716af9831f6e0e65c783
lukas.fischer <lukas.fischer@netnode.ch>
2023-01-03 10:18:51 +0100

    added Drupal crontab

--- nn/prod/deploy.sh
+++ nn/prod/deploy.sh
@@ -56,6 +56,9 @@ echo "Build Drupal theme –> DONE" | toilet -f term -F border --meta
 #find ./docroot/sites/default -name "*settings.php" -exec chmod 644 {} \;
 #find ./docroot/sites/default -name "*services.yml" -exec chmod 644 {} \;
 
+echo "RUN: install crontab" | toilet -f term -F border --meta
+crontab nn/prod/crontab
+
 echo "DEPLOYMENT FINISHED" | toilet --meta --filter border -t
 
 

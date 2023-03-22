6f32a87ebeff16a37f68d5b8e6dc7edda74e516c
lukas.fischer <lukas.fischer@netnode.ch>
2023-03-22 23:10:20 +0100

    improved start config

--- 0_netnode_start_config.sh
+++ 0_netnode_start_config.sh
@@ -7,3 +7,9 @@ vendor/drush/drush/drush theme:enable gin
 vendor/drush/drush/drush config-set system.theme admin gin -y
 vendor/drush/drush/drush config-set gin.settings classic_toolbar horizontal -y
 vendor/drush/drush/drush config-set user.settings register admin_only -y
+
+composer require drupal/gin_toolbar
+vendor/drush/drush/drush en gin_toolbar -y
+
+composer require drupal/gin_login
+vendor/drush/drush/drush en gin_login -y
